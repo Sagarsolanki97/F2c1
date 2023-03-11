308524e5fc537c80966876846c023f6b9c7ffa7f
Sagarsolanki97 <118007645+Sagarsolanki97@users.noreply.github.com>
2023-03-11 20:37:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function calculate() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var operator = document.getElementById("ddlOperators").value;
+    var result = null;
+  
+    if (operator == "+") {
+      result = num1 + num2;
+    } else if (operator == "-") {
+      result = num1 - num2;
+    } else if (operator == "*") {
+      result = num1 * num2;
+    } else if (operator == "/") {
+      result = num1 / num2;
+    }
+  
+    if (isNaN(num1) || isNaN(num2) || result == null) {
+      document.getElementById("result").innerHTML = "Invalid input";
+    } else {
+      document.getElementById("result").innerHTML = result;
+    }
+  }
+  function changeevent() {
+    console.log(document.getElementById("num1").value);
+  }
+
